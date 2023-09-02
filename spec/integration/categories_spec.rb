7e59db80347089883d548904dda0aa3fdbfd3729
Ashnaali3255 <ashnaalipk@gmail.com>
2023-09-02 20:06:24 +0500

    Fixed linters

--- spec/integration/categories_spec.rb
+++ spec/integration/categories_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe 'Authentication', type: :request do
   let(:user) { FactoryBot.create(:user) }
 
   before do
-    login_as user, scope: :user 
+    login_as user, scope: :user
   end
 
   it 'Should show categories for sign-in users' do
--- spec/integration/spendings_spec.rb
+++ spec/integration/spendings_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Authentication', type: :request do
   let(:user) { FactoryBot.create(:user) }
 
   before do
-    login_as(user, scope: :user) 
+    login_as(user, scope: :user)
   end
 
   it 'Should show categories for sign-in users' do
@@ -24,29 +24,29 @@ end
 
 RSpec.describe 'User', type: :feature do
   let(:user) { FactoryBot.create(:user) }
-  let(:category) { FactoryBot.create(:category, user: user, name: 'category') } 
+  let(:category) { FactoryBot.create(:category, user:, name: 'category') }
 
   before do
-    login_as(user, scope: :user) 
+    login_as(user, scope: :user)
   end
 
   scenario 'User creates a new transaction' do
     visit categories_path
-  
+
     expect(category).to be_valid
     visit "categories/#{category.id}"
 
     expect(page).to have_content('Create New Transaction')
-  
-    click_button 'Create New Transaction' 
-  
+
+    click_button 'Create New Transaction'
+
     fill_in 'Name', with: 'Burger'
     fill_in 'Amount', with: 3
     select 'category', from: 'selected_category_id'
 
     add_transaction_btn = find("input[type='submit']")
     add_transaction_btn.click
-  
+
     expect(page).to have_content(3)
-  end  
+  end
 end
